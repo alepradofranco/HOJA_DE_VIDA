5eecabf09b5203de2ee81e4254737a7558f9c810
alepradofranco <alepradofranco28@gmail.com>
2026-01-31 19:35:41 -0500

    Actualizo hoja de vida

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Instalar dependencias
-pip install -r requirements.txt
-
-# Aplicar migraciones
-python manage.py migrate
-
-# Recolectar archivos estáticos
-python manage.py collectstatic --noinput
